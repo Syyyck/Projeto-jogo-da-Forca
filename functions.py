06b5b9872f7d6e2e83158ce1c526e3fba0006ef4
Brayan Lima <brayandelimapfgmail.com>
2022-10-30 16:26:02 -0300

    Esboço 1

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
 import os 
 
 
+def dicas(dica1, dica2, dica3):
+    count=0
+
+def tentativa():
+    
--- main_game.py
+++ main_game.py
@@ -3,8 +3,12 @@ nameDesafiante=input("Digite o nome do desafiante: ")
 nameCompetidor=input("Digite o nome do competidor: ")
 os.system("cls")
 
+
+palavraChave=input("Digite a palavra:")
 dica1=input("Digite aqui a primeira dica: ")
 dica2=input("Digite aqui a segunda dica: ")
 dica3=input("Digite aqui a terceira dica: ")
 os.system("cls")
 
+print("*"*len(palavraChave))
+indicePalavra=list(palavraChave)
