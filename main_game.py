21b04a4926a9715f31054965e86cc382298ecf58
Brayan Lima <brayandelimapfgmail.com>
2022-10-30 13:54:26 -0300

    V.0 teste

---
+++ main_game.py
@@ -0,0 +1,10 @@
+import os 
+nameDesafiante=input("Digite o nome do desafiante: ")
+nameCompetidor=input("Digite o nome do competidor: ")
+os.system("cls")
+
+dica1=input("Digite aqui a primeira dica: ")
+dica2=input("Digite aqui a segunda dica: ")
+dica3=input("Digite aqui a terceira dica: ")
+os.system("cls")
+
