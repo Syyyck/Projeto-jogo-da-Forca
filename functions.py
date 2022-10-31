1469d84e319913e6cdab6f35780cbdf2012f1f4a
Brayan Lima <brayandelimapfgmail.com>
2022-10-31 00:07:04 -0300

    Trabalhando em funções2

--- functions.py
+++ functions.py
@@ -1,29 +1,44 @@
-import os 
+import os
+from time import sleep 
+
 
 
 def dicas(dica1, dica2, dica3):
-    c=0
-
-
-
-
+    contagemDica=0
+    return contagemDica
 
 def tentativa(palavraChave):
-    palavracodificada=("*"*len(palavraChave))
-    contagemErros=0
+    palavracodificada=list("X"*len(palavraChave))
     letraEscolhida=input("Digite a letra: ")
+    tentativasErradas=0
+    letraCorreta=[]
+
+    #acertou
+    for i in range(len(palavraChave)):
+        if palavraChave[i] == letraEscolhida:
+            palavracodificada[i] = letraEscolhida
+            letraCorreta.append(letraEscolhida)
+
+    palavraResultado = " ".join(palavracodificada) #Convert lista em str
     if letraEscolhida in palavraChave:
-        posicaoLetra_em_palavraChave=palavraChave.index(f'{letraEscolhida}')
-        posicaoLetra_em_palavraCod=palavraChave[posicaoLetra_em_palavraChave]
-        resultadoPalavra=palavracodificada.replace(f'{letraEscolhida}',f"{posicaoLetra_em_palavraCod}")
-        print("Você acertou!")
-        print(resultadoPalavra)
+        print("Você descobriu uma letra!")
     
-        
+    #errou
+    else:
+        print("Esta letra não pertence a palavra!")
+        tentativasErradas+=1
+        print("Você ainda possui: ",5-(tentativasErradas)," chances")
+    
+    return opcoes(palavraResultado),tentativasErradas
 
 
-
-def opcoes():
+def errosTentativas(letrasErradas):
+    numeroErros=letrasErradas
+    return numeroErros
+    
+    
+def opcoes(palavraResultado):
+    print(palavraResultado)
     print("Pedir Dica = [x]")
     print("Tentar Letra = [z]")
     opcao_XouZ=input("Digite aqui: ")
--- main_game.py
+++ main_game.py
@@ -1,47 +1,53 @@
 
-from functions import dicas
-from functions import tentativa
-from functions import opcoes
+from functions import *
 import os 
 
+def inicioConfig():
+    tentativasErradas=0
+
+    print("Você deseja iniciar um novo jogo? Sim(1) ou Não(2)")
+    perguntaIniciar=int(input(": "))
+    if perguntaIniciar == 1:
+        
+        '''nameDesafiante=input("Digite o nome do desafiante: ")
+        nameCompetidor=input("Digite o nome do competidor: ")
+        os.system("cls")'''
+
+        palavraChave=input("Digite a palavra:")
+        '''
+        dica1 = input("Digite aqui a primeira dica: ")
+        dica2 = input("Digite aqui a segunda dica: ")
+        dica3 = input("Digite aqui a terceira dica: ")
+        os.system("cls")'''
+
+        print("X"*len(palavraChave))
+        print("Pedir Dica = [x]")
+        print("Tentar Letra = [z]")
+        try:
+            opcao_XouZ=input("Digite aqui: ")# x para dica # z para tentar letra
+        except:
+            print("Digite um valor correto!")
 
 
-def rodada():
-    nameDesafiante=input("Digite o nome do desafiante: ")
-    nameCompetidor=input("Digite o nome do competidor: ")
-    os.system("cls")
+        while True:
+            if tentativasErradas < 5:
 
-    palavraChave=input("Digite a palavra:")
-    dica1 = input("Digite aqui a primeira dica: ")
-    dica2 = input("Digite aqui a segunda dica: ")
-    dica3 = input("Digite aqui a terceira dica: ")
-    os.system("cls")
+                #tentar letra
+                if opcao_XouZ == "z":
+                    tentativa(palavraChave)
 
-    while True:
-        contagemDica=0
-
-        print("*"*len(palavraChave))
-        opcoesDeJogo=opcoes ()
-
-       #dica
-        if opcoesDeJogo == "x": 
-            if contagemDica < 3:
-                dicas(dica1,dica2,dica3)
-                print("Agora você deve obrigatóriamente tentar uma letra!")
-                tentativa(palavraChave)
-                contagemDica+=1
+                #dica
+                '''if opcao_XouZ == "x": 
+                    dicas(dica1, dica2, dica3)
+                    if contagemDica < 3:
+                        print("Agora você deve obrigatóriamente tentar uma letra!")
+                        tentativa(palavraChave)
+                        contagemDica+=1
+                    else:
+                        print ("As dicas foram esgotadas!")'''
+                        
             else:
-                print ("As dicas foram esgotadas!")
-                return opcoes()
-
-        #tentativa
-        if opcoesDeJogo == "z":
-            tentativa(palavraChave)
-            break
-rodada()
-
-                    
+                break
+inicioConfig()
 
             
-
-
